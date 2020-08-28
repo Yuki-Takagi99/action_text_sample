cc65cc47d292a9b4bbe214615ee00c4468a2d20a
yuki takagi <yuki99takagi@gmail.com>
2020-08-28 13:25:15 +0900

    Add: 5-5-2 Action Textの使い方

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -69,6 +69,6 @@ class MessagesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def message_params
-      params.fetch(:message, {})
+      params.require(:message).permit(:content)
     end
 end
